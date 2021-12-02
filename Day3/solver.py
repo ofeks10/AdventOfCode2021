387024d4790488a341d09ee18fdd0ce5d9bddb7a
ofeks10 <ofeks10@gmail.com>
2021-12-02 19:13:13 +0200

    day3 prep

---
+++ Day3/solver.py
@@ -0,0 +1,22 @@
+import requests
+
+AOC_SESSION = ""
+
+def get_data():
+    data = requests.get('https://adventofcode/2021/day/3/input', cookies={'session': AOC_SESSION})
+    # manipulation
+    return data
+
+
+def solve_q1(data):
+    pass
+
+
+def solve_q2(data):
+    pass
+
+
+if __name__ == '__main__':
+    data = get_data()
+    solve_q1(data)
+    solve_q2(data)
