f50b731ba9d593c4efec150b35989c5a4b3882d2
ofeks10 <ofeks10@gmail.com>
2021-12-10 11:56:03 +0200

    Changed the part1 to send only the remaining stacks to part 2, so in part 2 we'll do less work

--- Day10/solver.py
+++ Day10/solver.py
@@ -10,59 +10,45 @@ def get_data() -> List[str]:
     data : List[str] = requests.get('https://adventofcode.com/2021/day/10/input',
         cookies={'session': AOC_SESSION}).content.decode('utf-8').split('\n')[:-1]
     
-    # data = open('./Day10/example.in', 'r').read().split('\n')[:-1] 
-
-    # print(data)
-
     return data
 
 
-def solve_q1(data: List[str]) -> List[str]:
+def solve_q1(data: List[str]) -> List[List[str]]:
     total = 0
     helper = {'(': ')', '{': '}', '[': ']', '<': '>'}
     scores = {')': 3, '}': 1197, ']': 57, '>': 25137}
-    incomplete: List[str] = []
+    incomplete: List[List[str]] = []
     for line in data:
         stack: List[str] = []
         broke = False
+
         for c in line:
-            if c in '({[<':
+            if c in helper.keys():
                 stack.append(c)
             else:
-                last = stack.pop()
-                if helper[last] != c:
+                if helper[stack.pop()] != c:
                     total += scores[c]
                     broke = True
                     break
+
         if len(stack) > 0 and not broke:
-            incomplete.append(line)
+            incomplete.append(stack)
     
     print(total)
     return incomplete
                 
 
-def solve_q2(incomplete: List[str]):
-    helper = {'(': ')', '{': '}', '[': ']', '<': '>'}
-    scores = {')': 1, '}': 3, ']': 2, '>': 4}
+def solve_q2(incomplete: List[List[str]]):
+    scores = {'(': 1, '{': 3, '[': 2, '<': 4}
     auto_complete_scored: List[int] = []
     for line in incomplete:
-        stack: List[str] = []
-        for c in line:
-            if c in '({[<':
-                stack.append(c)
-            else:
-                last = stack.pop()
-        # print(stack)
         current_score = 0
-        while len(stack) > 0:
-            last = stack.pop()
+        for c in line[::-1]:
             current_score *= 5
-            current_score += scores[helper[last]]
+            current_score += scores[c]
         auto_complete_scored.append(current_score)
-        # print(auto_complete_scored)
     
     auto_complete_scored.sort()
-    print(auto_complete_scored)
     print(auto_complete_scored[len(auto_complete_scored) // 2])
 
 
