0b9f15f599a96980ab4db91533d2ed155d4445ff
ofeks10 <ofeks10@gmail.com>
2021-12-13 10:56:11 +0200

    Day 14 prep

---
+++ Day14/solver.py
@@ -0,0 +1,26 @@
+import os
+from typing import List
+import requests
+
+
+AOC_SESSION = os.environ['AOC_SESSION']
+
+
+def get_data() -> List[str]:
+    data : List[str] = requests.get('https://adventofcode.com/2021/day/7/input',
+        cookies={'session': AOC_SESSION}).content.decode('utf-8').split('\n')[:-1]
+
+    return data
+
+
+def solve_q1(data: List[str]):
+    pass
+
+def solve_q2(data: List[str]):
+    pass
+
+
+if __name__ == '__main__':
+    data = get_data()    
+    solve_q1(data)
+    solve_q2(data)
